6ef951dbffb1bbc6135ce867cd3774912dcd37cf
mr-shreyansh <shreyansh1881@kgpian.iitkgp.ac.in>
2024-11-10 23:41:09 +0530

    cors fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const CryptoJS = require('crypto-js')
 const axios = require('axios')
+const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
